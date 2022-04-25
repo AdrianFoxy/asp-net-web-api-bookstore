e117d983feb5d2cf914a03ebc35ef5d0edbcb0d1
BasdDff <tank12358@gmail.com>
2022-04-25 13:58:15 +0300

    cookie

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,8 +1,18 @@
 import axios from "axios"
+import Cookies from 'js-cookie'
 
 const $api = axios.create({
     withCredentials: true,
     baseURL: "https://localhost:44307/api"
 })
+
+// $api.interceptors.request.use((config) => {
+//     //config.headers?[".AspNetCore.Session"] = ``
+//
+//     if (Cookies.get(".AspNetCore.Session")) {
+//         // @ts-ignore
+//         config.headers['.AspNetCore.Session']= `${Cookies.get('token')}`
+//     }
+// })
 
 export default $api
--- src/redux/actions/cart.ts
+++ src/redux/actions/cart.ts
@@ -2,37 +2,42 @@ import {AppDispatch} from "../index";
 import {cartSlice} from "../reducers/CartSlice";
 import {ICart} from "../../types/ICart";
 import $api from "../../http";
-
-
-// export const addProduct = (productId: number) => async (dispatch: AppDispatch) => {
-//     try {
-//         const response = await $api.post(`/Order/add-item-to-cart?id=${productId}`)
-//         console.log(response)
-//     } catch (e) {
-//         console.log(e)
-//     }
-// }
+import Cookies from 'js-cookie'
 
 export const addProduct = (productId: number) => async (dispatch: AppDispatch) => {
     try {
-        const request = new XMLHttpRequest();
-        request.open("POST", `https://localhost:44307/api/Order/add-item-to-cart?id=${productId}`)
-        request.withCredentials = true
-        request.responseType = "text"
-        request.onload = function () {
-            const response = request.response;
-            console.log(response)
-        };
-        request.send();
+        console.log(document.cookie)
+        console.log(Cookies.get(".AspNetCore.Session"))
+        const response = await $api.post(`/Order/add-item-to-cart?id=${productId}`)
+        console.log(response)
+        console.log(Cookies.get(".AspNetCore.Session"))
+        dispatch(cartSlice.actions.setCart(response.data))
     } catch (e) {
         console.log(e)
     }
 }
 
+// export const addProduct = (productId: number) => async (dispatch: AppDispatch) => {
+//     try {
+//         const request = new XMLHttpRequest();
+//         request.open("POST", `https://localhost:44307/api/Order/add-item-to-cart?id=${productId}`)
+//         request.withCredentials = true
+//         request.responseType = "text"
+//         request.onload = function () {
+//             const response = request.response;
+//             console.log(response)
+//         };
+//         request.send();
+//     } catch (e) {
+//         console.log(e)
+//     }
+// }
+
 export const fetchCart = () => async (dispatch: AppDispatch) => {
     try {
-        const response = await $api.get(`/Order/get-shopping-cart`)
-        console.log(response)
+        const response = await $api.get(`/Order/get-shopping-cart`, {withCredentials: true,})
+        console.log(response.config)
+        dispatch(cartSlice.actions.setCart(response.data))
     } catch (e) {
 
     }
