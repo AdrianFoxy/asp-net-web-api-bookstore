e23768caaef6ed6db268c7c7074f7506bc603df2
BasdDff <71138035+BasdDff@users.noreply.github.com>
2022-04-28 23:04:30 +0300

    Update ProductForm.tsx
    Update ProductForm.tsx

--- src/components/Admin/ProductForm.tsx
+++ src/components/Admin/ProductForm.tsx
@@ -28,19 +28,6 @@ const Input = styled('input')({
     display: 'none',
 });
 
-const names = [
-    'Oliver Hansen',
-    'Van Henry',
-    'April Tucker',
-    'Ralph Hubbard',
-    'Omar Alexander',
-    'Carlos Abbott',
-    'Miriam Wagner',
-    'Bradley Wilkerson',
-    'Virginia Andrews',
-    'Kelly Snyder',
-];
-
 function getStyles(name: string, personName: string[], theme: Theme) {
     return {
         fontWeight:
